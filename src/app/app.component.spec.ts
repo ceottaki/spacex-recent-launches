846b74dbbfbefc89c8c96c3dcc05bae5f18eece4
Felipe Ceotto <fceotto@iowaycorp.com>
2020-12-15 20:23:50 +0000

    App component tests.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
+import { LaunchCollectionComponent } from './launch-collection/launch-collection.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [HttpClientModule, RouterTestingModule],
+      declarations: [AppComponent, LaunchCollectionComponent],
+      providers: [HttpClient],
     }).compileComponents();
   });
 
@@ -20,16 +20,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'spacex-recent-launches'`, () => {
+  it(`should have number of launches set to 10`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('spacex-recent-launches');
+    expect(app.numLaunches).toEqual(10);
   });
 
-  it('should render title', () => {
+  it('should render brand title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('spacex-recent-launches app is running!');
+    expect(compiled.querySelector('div.navbar-brand').textContent).toContain(
+      'SpaceX Recent Launches'
+    );
   });
 });
